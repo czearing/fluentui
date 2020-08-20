15c8edfeee044b75c824903a44654abc8e3c5588
Caleb Zearing <czearing@outlook.com>
2020-08-20 11:31:28 -0700

    Updating PivotItem to a function component.

--- packages/react-tabs/src/components/Pivot/PivotItem.tsx
+++ packages/react-tabs/src/components/Pivot/PivotItem.tsx
@@ -1,20 +1,45 @@
 import * as React from 'react';
-import { getNativeProps, divProperties, initializeComponentRef, warnDeprecations } from '@uifabric/utilities';
+import { getNativeProps, divProperties, initializeComponentRef } from '@uifabric/utilities';
+import { useWarnings } from '@uifabric/react-hooks';
 import { IPivotItemProps } from './PivotItem.types';
 
 const COMPONENT_NAME = 'PivotItem';
 
-export class PivotItem extends React.Component<IPivotItemProps, {}> {
-  constructor(props: IPivotItemProps) {
-    super(props);
+// export const useFloatingSuggestionItems = <T extends {}>(
+//   floatingSuggestionItems: T[],
+//   focusSuggestionIndex?: number,
+//   isSuggestionsVisible?: boolean,
+// ) => {
 
-    initializeComponentRef(this);
-    warnDeprecations(COMPONENT_NAME, props, {
-      linkText: 'headerText',
+export const PivotItem: React.FunctionComponent = (props: IPivotItemProps) => {
+  useDebugWarning(props);
+  return <div {...getNativeProps(props, divProperties)}>{props.children}</div>;
+};
+
+function useDebugWarning(props: IPivotItemProps) {
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks -- build-time conditional
+    useWarnings({
+      name: COMPONENT_NAME,
+      props,
+      deprecations: {
+        linkText: 'headerText',
+      },
     });
   }
-
-  public render(): JSX.Element {
-    return <div {...getNativeProps(this.props, divProperties)}>{this.props.children}</div>;
-  }
 }
+
+// export class PivotItem extends React.Component<IPivotItemProps, {}> {
+//   constructor(props: IPivotItemProps) {
+//     super(props);
+
+//     initializeComponentRef(this);
+//     warnDeprecations(COMPONENT_NAME, props, {
+//       linkText: 'headerText',
+//     });
+//   }
+
+//   public render(): JSX.Element {
+//     return <div {...getNativeProps(this.props, divProperties)}>{this.props.children}</div>;
+//   }
+// }
